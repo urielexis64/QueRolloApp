3016fc22b5a7e86d5eb3c676ea1d93141004fb21
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-01-03 19:25:59 -0700

    Escuchadores agregados a los TextView "Iniciar sesión" y "Nueva Cuenta"

--- app/src/main/java/com/example/querolloapp/LoginActivity.java
+++ app/src/main/java/com/example/querolloapp/LoginActivity.java
@@ -5,17 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button btnLogin, btnPhoneLogin;
+    private EditText txtUserEmail, txtUserPassword;
+    private TextView lblNewAcount,lblForgetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeFields();
+
+        lblNewAcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void initializeFields() {
+        btnLogin = findViewById(R.id.login_button);
+        btnPhoneLogin = findViewById(R.id.phone_login_button);
+
+        txtUserEmail = findViewById(R.id.login_email);
+        txtUserPassword = findViewById(R.id.login_password);
+        lblNewAcount = findViewById(R.id.need_new_account_link);
+        lblForgetPassword = findViewById(R.id.forget_password_link);
     }
 
     @Override
@@ -23,18 +48,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
 
         if(currentUser!=null){
-            SendUserToMainActivity();
+            sendUserToMainActivity();
         }
     }
 
-    private void SendUserToMainActivity() {
+    private void sendUserToMainActivity() {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntent);
     }
 
-    public void new_account(View view) {
-        Intent loginIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-        startActivity(loginIntent);
+    public void sendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
         overridePendingTransition(R.anim.zoomenter, R.anim.zoomexit);
     }
 }
--- app/src/main/java/com/example/querolloapp/RegisterActivity.java
+++ app/src/main/java/com/example/querolloapp/RegisterActivity.java
@@ -5,17 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button btnCreateAccount;
+    private EditText txtUserEmail, txtUserPassword;
+    private TextView lblAlreadyHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initializeFields();
+        lblAlreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUserToLoginActivity();
+            }
+        });
     }
 
-    public void login(View view) {
-        super.onBackPressed();
+    private void initializeFields() {
+        btnCreateAccount = findViewById(R.id.register_button);
+        txtUserEmail = findViewById(R.id.register_email);
+        txtUserPassword = findViewById(R.id.register_password);
+        lblAlreadyHaveAccount = findViewById(R.id.already_have_account_link);
+    }
 
+    public void sendUserToLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        overridePendingTransition(R.anim.zoomenter, R.anim.zoomexit);
     }
 }
